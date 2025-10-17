ae36d893cd9cec8f2c229c49f9c4345e61a3629f
Kuanysh <developerandroidkz@gmail.com>
2025-10-17 19:31:08 +0500

    doc: added php doc for models

--- app/Module/Ticket/Models/Ticket.php
+++ app/Module/Ticket/Models/Ticket.php
@@ -2,9 +2,25 @@
 
 namespace App\Module\Ticket\Models;
 
+use App\Module\Customer\Models\Customer;
+use App\Module\TicketStatus\Models\TicketStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $customer_id
+ * @property int $ticket_status_id
+ * @property Carbon|null $answered_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Customer|null $customer
+ * @property-read TicketStatus|null $ticketStatus
+ */
 class Ticket extends Model
 {
     use HasFactory;
@@ -27,5 +43,15 @@ class Ticket extends Model
         return [
             'answered_at' => 'datetime',
         ];
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function ticketStatus(): BelongsTo
+    {
+        return $this->belongsTo(TicketStatus::class);
     }
 }
