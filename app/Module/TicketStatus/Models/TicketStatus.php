907487c240f7c7f2e9b5e64fce8d0ff00d73c8c2
Kuanysh <developerandroidkz@gmail.com>
2025-10-17 16:43:31 +0500

    feat: added Ticket status model and seeder

---
+++ app/Module/TicketStatus/Models/TicketStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Module\TicketStatus\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketStatus extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+}
